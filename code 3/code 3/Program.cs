c942e0169b2106e5f0f6266cfb1da9d8e27edc87
Ulviyyaaa <tu7s25j8q@code.edu.az>
2022-11-01 03:25:12 +0400

    task

---
+++ code 3/code 3/Program.cs
@@ -0,0 +1,11 @@
+﻿int num = 1275;
+int count = 0;
+for (int i = 0; i < num; i++)
+{
+    num /= 10;
+    if (num >0)
+    {
+        count++;
+    }
+}
+Console.WriteLine(count + 1);
---
+++ code1.js
@@ -0,0 +1,7 @@
+var num = 7;
+var fakt = 1;
+for (var i = 1; i<=num; i++)
+{
+    fakt *= i; //fakt = fakt*i
+}
+console.log(fakt);
